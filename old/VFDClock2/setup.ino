42188e9a59d5ad5aeabce6d222170958dee88ca8
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-09-11 15:04:07 +0900

    update

---
+++ old/VFDClock2/setup.ino
@@ -0,0 +1,36 @@
+void swUpdate() {
+  int val = analogRead(PIN_BUTTON_MODE);
+  if (val < 300) {  // SW1
+    if (pmillis[SW1_ON] == 0 || pmillis[SW1_OFF] != 0) {
+      pmillis[SW1_ON]  = millis();
+      pmillis[SW1_OFF] = 0;
+    }
+  } else if (pmillis[SW1_ON] != 0) {
+    if (pmillis[SW1_OFF] == 0)
+      pmillis[SW1_OFF] = millis();
+  }
+
+  val = analogRead(PIN_BUTTON_COUNT);
+  if (val < 300) {  // SW2
+    if (pmillis[SW2_ON] == 0 || pmillis[SW2_OFF] != 0) {
+      pmillis[SW2_ON]  = millis();
+      pmillis[SW2_OFF] = 0;
+    }
+  } else if (pmillis[SW2_ON] != 0) {
+    if (pmillis[SW2_OFF] == 0)
+      pmillis[SW2_OFF] = millis();
+  }
+}
+
+void setTime() {
+  switch (mode) {
+    case MODE_TIMESET_HOUR:
+      if (++dHH > 23) dHH = 0;
+      break;
+
+    case MODE_TIMESET_MIN:
+      if (++dMM > 59) dMM = 0;
+      break;
+  }
+}
+

5a0d4aaaae5a0cbd27faec1a2b891af61fb8e479
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-09-17 03:35:27 +0900

    update

--- VfdClock/Button.h
+++ VfdClock/Button.h
@@ -5,31 +5,29 @@
 #define BUTTON_H
 #include <Arduino.h>
 
-typedef unsigned long id_t;
-typedef uint8_t button_event_t;
+typedef unsigned long	id_t;
+typedef uint8_t		button_event_t;
+typedef uint8_t		count_t;
 
 class Button {
  public:
-  static const unsigned long DEBOUNCE        	=   10;
-  static const unsigned long LONG_PRESS_MSEC 	= 1000;
-  static const unsigned long REPEAT_MSEC     	=  100;
-  static const unsigned long MULTI_MSEC 	=  500;
+  static const unsigned long	DEBOUNCE        	=   10;
+  static const unsigned long	LONG_PRESS_MSEC 	= 1000;
+  static const unsigned long	REPEAT_MSEC     	=  100;
+  static const unsigned long	MULTI_MSEC		=  500;
 
-  static const button_event_t EVENT_NONE		= 0x00;
-  static const button_event_t EVENT_INTRRUPT		= 0x10;
-  static const button_event_t EVENT_PESSED		= 0x11;
-  static const button_event_t EVENT_RELEASED		= 0x12;
-  static const button_event_t EVENT_LONG_PRESSED 	= 0x02;
-  static const button_event_t EVENT_REPEAT		= 0x03;
+  static const button_event_t	EVENT_NONE		= 0x00;
+  static const button_event_t	EVENT_INTRRUPT		= 0x10;
+  static const button_event_t	EVENT_PESSED		= 0x11;
+  static const button_event_t	EVENT_RELEASED		= 0x12;
+  static const button_event_t	EVENT_LONG_PRESSED 	= 0x02;
+  static const button_event_t	EVENT_REPEAT		= 0x03;
 
-  static unsigned long	Num;
-  static Button		**BTN;
-
-  static void 		interruptHandler();
-  static void 		loopHandler();
+  static const unsigned long	NUM_MAX = 10;
+  static unsigned long		Num;
+  static Button			*Obj[];
 
   Button();
-  Button(byte pin, String name);
 
   void    	init(byte pin, String name);
 
@@ -41,29 +39,29 @@ class Button {
 
   String  	name();
   boolean 	value();
-  byte    	count();
+  count_t    	count();
+  count_t	multi_count();
   boolean	long_pressed();
   boolean	repeat();
-  uint8_t	multi_count();
 
   void    	print();
+  void    	print(boolean interrupt);
 
  private:
   id_t		_id;
-  String        _name;
-  byte           _pin;
-  boolean       _value;      // HIGH/LOW
-  boolean       _prev_value; // HIGH/LOW
-  unsigned long _first_press_start; // msec
-  unsigned long _press_start;       // msec
-  byte          _count;
-  boolean       _long_pressed;
-  boolean       _repeat;
-  uint8_t	_multi_count;
+  String	_name;
+  boolean	_is_enabled = false;
+  byte		_pin;
+  boolean	_value;      // HIGH/LOW
+  boolean	_prev_value; // HIGH/LOW
+  unsigned long	_first_press_start; // msec
+  unsigned long	_press_start;       // msec
+  count_t	_count;
+  count_t	_multi_count;
+  boolean	_long_pressed;
+  boolean	_repeat;
 
-  boolean       _is_enabled = true;
-
-  void pciSetup(byte pin);
+  void 		pciSetup(byte pin);
 };
 
 #endif
